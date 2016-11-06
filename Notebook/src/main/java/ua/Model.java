c7fcb80840a8c078d4bf130dc6ea8516d9358ce9
Andrii97 <Severinandrey97@gmail.com>
2016-11-06 21:28:24 +0200

    added MVC structure

---
+++ Notebook/src/main/java/ua/Model.java
@@ -0,0 +1,12 @@
+package ua;
+
+/**
+ * Created by andrii on 06.11.16.
+ */
+public class Model {
+
+
+    // The Program logic
+
+
+}
---
+++ Notebook/src/main/java/ua/View.java
@@ -0,0 +1,17 @@
+package ua;
+
+/**
+ * Created by andrii on 06.11.16.
+ */
+public class View {
+    // Text's constants
+    public static final String INPUT_INT_DATA = "Input INT value = ";
+    public static final String WRONG_INPUT_INT_DATA = "Wrong input! Repeat please! ";
+    public static final String OUR_INT = "INT value = ";
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+
+
+}
