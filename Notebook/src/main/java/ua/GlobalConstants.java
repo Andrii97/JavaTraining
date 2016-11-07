b21081780571f8d82c19a3bd465683cba41cfb30
Andrii97 <Severinandrey97@gmail.com>
2016-11-07 09:04:05 +0200

    fixed bugs

--- Notebook/src/main/java/ua/GlobalConstants.java
+++ Notebook/src/main/java/ua/GlobalConstants.java
@@ -3,15 +3,17 @@ package ua;
 /**
  * Created by andrii on 06.11.16.
  */
-public class GlobalConstants {
+public interface GlobalConstants {
     // «Andrii»
     String REGEX_NAME = "^[A-Z][a-z]+$";
     // «an_d.»
     String REGEX_NICKNAME = "^[a-zA-Z][a-zA-Z0-9-_\\.]{1,20}$";
     // «FRIENDS»
     String REGEX_GROUP = "FRIENDS|RELATIVES|WORK|EDUCATION";
+    // «(044)555-55-55»
+    String REGEX_HOME_PHONE = "^\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$";
     // «+38(044)555-55-55»
-    String REGEX_PHONE = "^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$";
+    String REGEX_MOBILE_PHONE = "^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$";
     // «nick@mail.com»
     String REGEX_EMAIL = "^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$";
     //
@@ -21,7 +23,7 @@ public class GlobalConstants {
     // «12b»
     String HOUSE_NUMBER = "^\\d+[a-zA-Z]*$";
     // «12»
-    String apartmentNumber = "^[\\d]+$";
+    String APARTMENT_NUMBER = "^[\\d]+$";
     // «DD.MM.YYYY»
     String DATE = "^(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}$";
 }
